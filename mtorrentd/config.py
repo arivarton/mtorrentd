5a03c9350f0bd6d72bff61c9279e0bbac1cb31f7
arivarton <accounts@arivarton.com>
2018-04-22 12:08:03 +0000

    Validating config values when loading config. Fixes #19

--- mtorrentd/config.py
+++ mtorrentd/config.py
@@ -4,21 +4,30 @@ import yaml
 
 from .paths import CONFIG_PATHS, CONFIG_NAMES
 from .default_config_values import CONFIG, SITES
+from .core import validate_url
+
+CONFIG_SETTINGS = {
+    'sites': {
+    'required_values': ['login_required', 'page_path', 'search_path', 'url'],
+    'login_required_values': ['username', 'password'],
+    }
+    'config': {
+    }
+}
+
 
 def handle_undefined_sites_value(selected_config, config_selection) -> dict:
     if config_selection is 'sites':
-        required_values = ['login_required', 'page_path', 'search_path', 'url']
-        login_required_values = ['username', 'password']
         for site, site_values in selected_config.items():
             # Check required values
-            for value in required_values:
+            for value in CONFIG_SETTINGS[config_selection]['required_values']:
                 if value not in site_values.keys():
                     print(value, 'must be specified under configuration for', site + '.')
                     exit(78)
             # Set username and password to None if not defined in a site with
             # login_required: True
             if site_values['login_required']:
-                for value in login_required_values:
+                for value in CONFIG_SETTINGS[config_selection]['login_required_values']:
                     if value not in site_values.keys():
                         selected_config[site][value] = None
     elif config_selection is 'config':
@@ -27,7 +36,26 @@ def handle_undefined_sites_value(selected_config, config_selection) -> dict:
     return selected_config
 
 
+def validate_config_values(selected_config, config_selection) -> bool:
+    if config_selection is 'sites':
+        for key, values in selected_config.items():
+            try:
+                validate_url(values['url'])
+                validate_url(values['page_path'], path=True)
+                validate_url(values['search_path'], path=True)
+                validate_url(values.get('append_path', 'Not required'), path=True)
+            except ValueError as err:
+                print('Error when validating config value for %s: %s' % (key, err))
+    elif config_selection is 'config':
+        for key, values in selected_config.items():
+            try:
+                validate_url(values.get('watch_dir', 'Not required'), path=True)
+            except ValueError as err:
+                print('Error when validating config value for %s: %s' % (key, err))
+
+
 def load_config(config_selection) -> dict:
+    # Load default config
     if config_selection == 'config':
         selected_config = CONFIG
     elif config_selection == 'sites':
@@ -35,6 +63,7 @@ def load_config(config_selection) -> dict:
     else:
         raise ValueError('Config selection value is not supported.')
 
+    # Load user config
     try:
         with open(os.path.join(CONFIG_PATHS['user'], CONFIG_NAMES[config_selection]), 'r') as config_readout:
             loaded_config = yaml.load(config_readout)
@@ -42,9 +71,11 @@ def load_config(config_selection) -> dict:
                 # loop is required for sites to not replace the dictionary
                 # inside the dictionary when updating
                 if config_selection == 'sites':
-                    for site in selected_config:
-                        if site in loaded_config:
+                    for site in loaded_config:
+                        if site in selected_config:
                             selected_config[site].update(loaded_config[site])
+                        else:
+                            selected_config.update({site: loaded_config[site]})
                 else:
                     selected_config.update(loaded_config)
             except yaml.YAMLError as err:
@@ -53,4 +84,7 @@ def load_config(config_selection) -> dict:
     except FileNotFoundError:
         pass
 
-    return handle_undefined_sites_value(selected_config, config_selection)
+    selected_config = handle_undefined_sites_value(selected_config, config_selection)
+    validate_config_values(selected_config, config_selection)
+
+    return selected_config
